b05a83c88aedcac81793540affbc7ef86c9b7106
erickbp <erickbp@gmail.com>
2015-08-27 19:53:15 -0400

    First commit

---
+++ Robot1/Robot1/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Robot1
+{
+  
+    //Para acordarme luego. Es un partial class, la otra parte hereda de Page
+    public sealed partial class MainPage
+    {
+        private BackgroundWorker _worker;
+        private CoreDispatcher _dispatcher;
+
+        private bool _finish;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Loaded += MainPage_Loaded;
+
+            Unloaded += MainPage_Unloaded;
+        }
+
+        private void MainPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            _dispatcher = CoreWindow.GetForCurrentThread().Dispatcher;
+
+            _worker = new BackgroundWorker();
+            _worker.DoWork += DoWork;
+            _worker.RunWorkerAsync();
+        }
+
+        private void MainPage_Unloaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            _finish = true;
+        }
+
+        private async void DoWork(object sender, DoWorkEventArgs e)
+        {
+            var driver = new TwoMotorsDriver(new Motor(27, 22), new Motor(5, 6));
+            var ultrasonicDistanceSensor = new UltrasonicDistanceSensor(23, 24);
+
+            await WriteLog("Moving forward");
+
+            while (!_finish)
+            {
+                driver.MoveForward();
+
+                await Task.Delay(200);
+
+                var distance = await ultrasonicDistanceSensor.GetDistanceInCmAsync(1000);
+                if (distance > 35.0)
+                    continue;
+
+                await WriteLog($"Obstacle found at {distance} cm or less. Turning right");
+
+                await driver.TurnRightAsync();
+
+                await WriteLog("Moving forward");
+            }
+        }
+
+        private async Task WriteLog(string text)
+        {
+            await _dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                Log.Text += $"{text} | ";
+            });
+        }
+    }
+}
---
+++ Robot1/Robot1/TwoMotorsDriver.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Robot1
+{
+    public class TwoMotorsDriver
+    {
+        private readonly Motor _leftMotor;
+        private readonly Motor _rightMotor;
+
+        public TwoMotorsDriver(Motor leftMotor, Motor rightMotor)
+        {
+            _leftMotor = leftMotor;
+            _rightMotor = rightMotor;
+        }
+
+        public void Stop()
+        {
+            _leftMotor.Stop();
+            _rightMotor.Stop();
+        }
+
+        public void MoveForward()
+        {
+            _leftMotor.MoveForward();
+            _rightMotor.MoveForward();
+        }
+
+        public void MoveBackward()
+        {
+            _leftMotor.MoveBackward();
+            _rightMotor.MoveBackward();
+        }
+
+        public async Task TurnRightAsync()
+        {
+            _leftMotor.MoveForward();
+            _rightMotor.MoveBackward();
+
+            await Task.Delay(TimeSpan.FromMilliseconds(250));
+
+            _leftMotor.Stop();
+            _rightMotor.Stop();
+        }
+
+        public async Task TurnLeftAsync()
+        {
+            _leftMotor.MoveBackward();
+            _rightMotor.MoveForward();
+
+            await Task.Delay(TimeSpan.FromMilliseconds(250));
+
+            _leftMotor.Stop();
+            _rightMotor.Stop();
+        }
+    }
+
+}
