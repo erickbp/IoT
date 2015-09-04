0dfe27d9908ac71cce768129a91932d7f85bec5e
erickbp <erickbp@gmail.com>
2015-09-03 22:48:49 -0400

    -Stepper motor

---
+++ Stepper Motor/Stepper Motor/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Stepper_Motor
+{
+    public sealed partial class MainPage
+    {
+        private readonly Uln2003Driver _uln2003Driver;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _uln2003Driver = new Uln2003Driver(26, 13, 6, 5);
+
+            Loaded += MainPage_Loaded;
+            Unloaded += MainPage_Unloaded;
+        }
+        
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                await _uln2003Driver.TurnAsync(90, TurnDirection.Left);
+                await _uln2003Driver.TurnAsync(90, TurnDirection.Right);
+            });
+        }
+
+        private void MainPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _uln2003Driver?.Dispose();
+        }
+    }
+}
