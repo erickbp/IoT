1f2958696d36fdc64cc0da10fa1912e6d2d663ba
erickbp <erickbp@gmail.com>
2016-06-25 16:29:08 -0400

    anja

--- RobotSpeech/RobotSpeech/MainWindow.xaml.cs
+++ RobotSpeech/RobotSpeech/MainWindow.xaml.cs
@@ -110,7 +110,7 @@ namespace RobotSpeech
         private void OnIntent(object sender, SpeechIntentEventArgs e)
         {
             WriteToLog(e.Payload);
-            //_queueClient.Send(new BrokeredMessage(new MemoryStream(Encoding.UTF8.GetBytes(e.Payload))));
+            _queueClient.Send(new BrokeredMessage(new MemoryStream(Encoding.UTF8.GetBytes(e.Payload))));
         }
 
         private void StartOnClick(object sender, RoutedEventArgs e)
